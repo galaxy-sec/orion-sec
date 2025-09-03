6590f54ae7e956e106cb26b316d654d86c478647
wukong <sec-wukong@outlook.com>
2025-09-03 12:06:26 +0800

    init

--- src/error.rs
+++ src/error.rs
@@ -1,20 +1,21 @@
+use derive_more::From;
 use orion_error::{StructError, UvsReason};
 use serde_derive::Serialize;
 use thiserror::Error;
 
-#[derive(Debug, PartialEq, Serialize, Error)]
-pub enum SecErrReason {
-    #[error("sensivive msg {0}")]
-    SensitiveMsg(String),
+#[derive(Debug, PartialEq, Serialize, Error, From)]
+pub enum OrionSecReason {
+    #[error("{0}")]
+    Sec(SecReason),
     #[error("{0}")]
     Uvs(UvsReason),
 }
 
-pub type SecError = StructError<SecErrReason>;
-pub type SecResult<T> = Result<T, SecError>;
-
-impl From<UvsReason> for SecErrReason {
-    fn from(value: UvsReason) -> Self {
-        SecErrReason::Uvs(value)
-    }
+#[derive(Debug, PartialEq, Serialize, Error)]
+pub enum SecReason {
+    #[error("sensivive msg {0}")]
+    SensitiveMsg(String),
 }
+
+pub type SecError = StructError<OrionSecReason>;
+pub type SecResult<T> = Result<T, SecError>;
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@ mod error;
 mod load;
 pub mod sec;
 pub mod types;
-pub use error::{SecErrReason, SecError, SecResult};
+pub use error::{OrionSecReason, SecError, SecReason, SecResult};
 pub use load::{load_sec_dict, load_secfile};
